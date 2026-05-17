652211b8bcff37c0cc35bb5eef6e85435e4a16b7
Draemiz <draemizgx1@gmail.com>
2026-05-17 10:35:09 -0500

    Implementacion de manejo de excepciones y propagacion en Java, ccreamor en primer lugar el archhivo de texto con el logback

---
+++ src/test/java/com/krakedev/taller/TallerExcepcionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerExcepcionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
